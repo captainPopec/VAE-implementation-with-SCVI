4180bf4a6630f3341ca19a0cd399e041cf0829cb
David Spiljak <spiljak@login2.cluster.embl.de>
2026-05-21 15:22:04 +0200

    clean up repo and reorganize into the first github repo

---
+++ src/modules/__init__.py
@@ -0,0 +1,4 @@
+from .custom_module import *
+from .scvi_module import *
+
+__all__ = ["custom_module", "scvi_module"]
---
+++ src/wrapper/__init__.py
@@ -0,0 +1,4 @@
+from .custom_wrapper import *
+from .scvi_wrapper import *
+
+__all__ = ["custom_wrapper", "scvi_wrapper"]
